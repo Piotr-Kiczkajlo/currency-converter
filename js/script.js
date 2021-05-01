35c310c3066c8b118e106a1f6192baf4d735209d
Piotr Kiczkajło <kiczkajlo.p@gmail.com>
2021-05-01 15:43:13 +0200

    functions added

--- js/script.js
+++ js/script.js
@@ -1,36 +1,44 @@
-let currencyPLN = document.querySelector(".js-valuePL");
-let currencyUSD = document.querySelector(".js-valueUSD");
-let currencyGBP = document.querySelector(".js-valueGBP");
-let currencyEUR = document.querySelector(".js-valueEUR");
-let currencyOption = document.querySelector(".js-valueOption");
-let exchangeButton = document.querySelector(".js-form");
-let resultValue = document.querySelector(".js-result");
+{
+    const calculate = (amoutPLN, valueOption) => {
+        const usdElement = document.querySelector(".js-valueUSD");
+        const gbpElement = document.querySelector(".js-valueGBP");
+        const eurElement = document.querySelector(".js-valueEUR");
+        const rateUSD = usdElement.value;
+        const rateGBP = gbpElement.value;
+        const rateEUR = eurElement.value;
 
-
-exchangeButton.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    let valuePLN = currencyPLN.value;
-    let valueUSD = currencyUSD.value;
-    let valueGBP = currencyGBP.value;
-    let valueEUR = currencyEUR.value;
-    let valueOption = currencyOption.value;
-    let result;
-
-    switch (valueOption) {
-        case "usd":
-            result = valuePLN / valueUSD;
-            resultValue.innerText = result.toFixed(2) + " $";
-            break;
-        case "gbp":
-            result = valuePLN / valueGBP;
-            resultValue.innerText = result.toFixed(2) + " £";
-            break;
-        case "eur":
-            result = valuePLN / valueEUR;
-            resultValue.innerText = result.toFixed(2) + " €";
+        switch (valueOption) {
+            case "$":
+                return amoutPLN / rateUSD;
+            case "£":
+                return amoutPLN / rateGBP;
+            case "€":
+                return amoutPLN / rateEUR;
+        }
     }
-    exchangeButton.addEventListener("reset", () => {
-        resultValue.innerText = 0;
-    });
-});
+    const formSubmit = (event) => {
+        event.preventDefault();
+
+        const amoutElement = document.querySelector(".js-valuePL");
+        const amoutPLN = +amoutElement.value;
+        const currencyOption = document.querySelector(".js-valueOption");
+        const valueOption = currencyOption.value;
+        const result = calculate(amoutPLN, valueOption);
+
+        changeTextResult(result, valueOption);
+    }
+    const formReset = () => {
+        const resultElement = document.querySelector(".js-result");
+        resultElement.innerText = 0;
+    }
+    const changeTextResult = (result, valueOption) => {
+        const resultElement = document.querySelector(".js-result");
+        resultElement.innerText = `${result.toFixed(2)} ${valueOption}`;
+    }
+    const init = () => {
+        const formAction = document.querySelector(".js-form");
+        formAction.addEventListener("submit", formSubmit);
+        formAction.addEventListener("reset", formReset);
+    }
+    init();
+}
